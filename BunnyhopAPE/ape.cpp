dbe1775803b9818e1a332244fef6bb8552d6ea1b
rumour <byriots@gmail.com>
2021-04-17 13:50:01 +0200

    Modified to fix clientsided prediction errors while using new landfix

--- BunnyhopAPE/ape.cpp
+++ BunnyhopAPE/ape.cpp
@@ -11,13 +11,22 @@ BYTE g_nopBuffer[6] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
 bool g_bPatched;
 int g_iOldState;
 
+DWORD g_jumpHeightPtrPtr;
+DWORD g_jumpHeightPtr;
+DWORD g_valuePtr;
+DWORD g_floatOffsetPtrPtr;
+DWORD g_oldFloatOffsetPtr;
+DWORD g_floatOffsetPtr;
+
+double g_oldJumpHeight;
+double g_newJumpHeight = 93592.0;
+
 void Error(char* text)
 {
 	MessageBox(0, text, "ERROR", 16);
 	ExitProcess(0);
 }
-
-void UpdateConlole()
+void UpdateConsole()
 {
 	system("cls");
 	printf("Use SCROLL LOCK to toggle prediction.\nPrediction status: %s\n", g_bPatched ? "ON" : "OFF");
@@ -26,16 +35,39 @@ void UpdateConlole()
 void EnablePrediction()
 {
 	ReadProcessMemory(g_hProcess, g_pJumpPrediction, g_patchedBuffer, 6, NULL);
+	ReadProcessMemory(g_hProcess, LPVOID(g_jumpHeightPtrPtr), &g_jumpHeightPtr, sizeof(g_jumpHeightPtr), NULL);
+	ReadProcessMemory(g_hProcess, LPVOID(g_jumpHeightPtr), &g_oldJumpHeight, sizeof(g_oldJumpHeight), NULL);
+	ReadProcessMemory(g_hProcess, LPVOID(g_floatOffsetPtrPtr), &g_oldFloatOffsetPtr, sizeof(g_oldFloatOffsetPtr), NULL);
+
+	DWORD Old;
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_floatOffsetPtrPtr) & ~0xfff), 0x4000, PAGE_EXECUTE_READWRITE, &Old);
+	WriteProcessMemory(g_hProcess, LPVOID(g_floatOffsetPtrPtr), &g_valuePtr, sizeof(g_valuePtr), NULL);
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_floatOffsetPtrPtr) & ~0xfff), 0x4000, Old, &Old);
+
+	ReadProcessMemory(g_hProcess, LPVOID(g_floatOffsetPtrPtr), &g_floatOffsetPtr, sizeof(g_floatOffsetPtr), NULL);
+
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_jumpHeightPtr) & ~0xfff), 0x4000, PAGE_EXECUTE_READWRITE, &Old);
+	WriteProcessMemory(g_hProcess, LPVOID(g_jumpHeightPtr), &g_newJumpHeight, sizeof(g_newJumpHeight), NULL);
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_jumpHeightPtr) & ~0xfff), 0x4000, Old, &Old);
 
 	if (WriteProcessMemory(g_hProcess, g_pJumpPrediction, g_nopBuffer, 6, NULL))
 		g_bPatched = true;
 	else Error("Game is already patched or signatures are outdated!");
 
-	UpdateConlole();
+	UpdateConsole();
 }
 
 void DisablePrediction(bool notify = true)
 {
+	DWORD Old;
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_jumpHeightPtr) & ~0xfff), 0x4000, PAGE_EXECUTE_READWRITE, &Old);
+	WriteProcessMemory(g_hProcess, LPVOID(g_jumpHeightPtr), &g_oldJumpHeight, sizeof(g_oldJumpHeight), NULL);
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_jumpHeightPtr) & ~0xfff), 0x4000, Old, &Old);
+
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_floatOffsetPtrPtr) & ~0xfff), 0x4000, PAGE_EXECUTE_READWRITE, &Old);
+	WriteProcessMemory(g_hProcess, LPVOID(g_floatOffsetPtrPtr), &g_oldFloatOffsetPtr, sizeof(g_oldFloatOffsetPtr), NULL);
+	VirtualProtectEx(g_hProcess, LPVOID(DWORD(g_floatOffsetPtrPtr) & ~0xfff), 0x4000, Old, &Old);
+
 	if (WriteProcessMemory(g_hProcess, g_pJumpPrediction, g_patchedBuffer, 6, NULL))
 	{
 		if (notify)
@@ -43,7 +75,7 @@ void DisablePrediction(bool notify = true)
 	}
 	else Error("Memory access violation!");
 
-	UpdateConlole();
+	UpdateConsole();
 }
 
 bool WINAPI ConsoleHandler(DWORD dwCtrlType)
@@ -72,7 +104,7 @@ int main()
 	DWORD pClient;
 	while (1)
 	{
-		pClient = (DWORD)GetModuleHandleExtern(processID, "client.dll");
+		pClient = DWORD(GetModuleHandleExtern(processID, "client.dll"));
 		if (pClient) break;
 		Sleep(100);
 	}
@@ -87,9 +119,12 @@ int main()
 		Error("-insecure key is missing!");
 
 	g_pJumpPrediction = (BYTE*)(FindPatternEx(g_hProcess, pClient, 0x200000, (PBYTE)"\x85\xC0\x8B\x46\x08\x0F\x84\x00\xFF\xFF\xFF\xF6\x40\x28\x02\x0F\x85\x00\xFF\xFF\xFF", "xxxxxxx?xxxxxxxxx?xxx")) + 15;
+	g_jumpHeightPtrPtr = (FindPatternEx(g_hProcess, pClient, 0x200000, (PBYTE)"\xDD\x05\x00\x00\x00\x00\xD9\xFA\xD8\x4D\xFC\xD9\x59\x48", "xx????xxxxxxxx")) + 2;
+	g_valuePtr = (FindPatternEx(g_hProcess, pClient, 0x200000, (PBYTE)"\xC7\x45\x00\x00\x00\x00\x00\x0F\x87\x00\x00\x00\x00\xE8\x00\x00\x00\x00\x8B\xC8", "xx?????xx????x????xx")) + 3;
+	g_floatOffsetPtrPtr = (FindPatternEx(g_hProcess, pClient, 0x200000, (PBYTE)"\xF3\x0F\x5C\x05\x00\x00\x00\x00\xF3\x0F\x11\x45\x00\xF3\x0F\x10\x80\x00\x00\x00\x00\xF3\x0F\x11\x45\x00", "xxxx????xxxx?xxxx????xxxx?")) + 4;
 
-	SetConsoleCtrlHandler((PHANDLER_ROUTINE)&ConsoleHandler, true);
-	UpdateConlole();
+	SetConsoleCtrlHandler(PHANDLER_ROUTINE(&ConsoleHandler), true);
+	UpdateConsole();
 
 	while (1)
 	{
